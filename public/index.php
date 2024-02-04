02dcd3a8e2ffff2d6b327963aa0e45a146388299
klon03 <wachowski.kajetan@gmail.com>
2024-02-04 18:00:00 +0100

    ip

--- public/index.php
+++ public/index.php
@@ -25,7 +25,7 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://kwachowski.pl')
+        ->withHeader('Access-Control-Allow-Origin', 'https://kwachowski.pl')
         ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST');
 });
