739486754035e19ad3a4dc12818413add14b4bb6
klon03 <wachowski.kajetan@gmail.com>
2024-02-04 13:44:00 +0100

    cors edit

--- public/index.php
+++ public/index.php
@@ -23,11 +23,22 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 
 // Middleware do obsługi CORS
 $app->add(function ($request, $handler) {
-    $response = $handler->handle($request);
-    return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
-        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST');
+    $allowedOrigins = [
+        'http://127.0.0.1:5500',
+        'https://kwachowski.pl/'
+    ];
+
+    $origin = $request->getHeaderLine('Origin');
+
+    if (in_array($origin, $allowedOrigins)) {
+        $response = $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', $origin)
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST');
+    }
+
+    return new Response('Not allowed by CORS', 403);
 });
 
 
